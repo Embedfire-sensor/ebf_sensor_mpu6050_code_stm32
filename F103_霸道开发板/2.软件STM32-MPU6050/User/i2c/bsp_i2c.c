2f5387bde62a42c2794a6e45e73f58adbfc83cc8
Yuhailong <2816850683@qq.com>
2020-06-30 18:02:24 +0800

    修改了IIC的使用引脚，之前程序使用的引脚与硬件不匹配

--- F103_霸道开发板/2.软件STM32-MPU6050/User/i2c/bsp_i2c.c
+++ F103_霸道开发板/2.软件STM32-MPU6050/User/i2c/bsp_i2c.c
@@ -26,8 +26,8 @@
 /* 定义I2C总线连接的GPIO端口, 用户只需要修改下面4行代码即可任意改变SCL和SDA的引脚 */
 #define GPIO_PORT_I2C	GPIOB			/* GPIO端口 */
 #define RCC_I2C_PORT 	RCC_APB2Periph_GPIOB		/* GPIO端口时钟 */
-#define I2C_SCL_PIN		GPIO_Pin_1			/* 连接到SCL时钟线的GPIO */
-#define I2C_SDA_PIN		GPIO_Pin_5			/* 连接到SDA数据线的GPIO */
+#define I2C_SCL_PIN		GPIO_Pin_6			/* 连接到SCL时钟线的GPIO */
+#define I2C_SDA_PIN		GPIO_Pin_7			/* 连接到SDA数据线的GPIO */
 
 
 
